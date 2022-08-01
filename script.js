af2a97dbf839861f7cb846913c38c473011298d6
Thomas Anderson <ahmetacar1111@gmail.com>
2022-08-01 06:33:00 -0400

    Add necessary functions in displayController module

--- script.js
+++ script.js
@@ -75,8 +75,10 @@ const Gameboard = (function() {
     return currentPlayer;
   }
   
+  // Magic Happens Below!
   const {player1,player2} = _getPlayer()
   const _updateGameBoard = (event) => {
+    // We set data-index attribute to find the clicked box and add it to gameBoard
     const myIndex = event.target.getAttribute("data-index");
     let currentPlayer = _currentPlayer(player1,player2);
     currentPlayer.addMark(myIndex)    
@@ -88,7 +90,7 @@ const Gameboard = (function() {
 
     // Try gameover // Continue from here
     let isGameOver = displayController.gameOver();
-    if (isGameOver) {
+    if (isGameOver.gameOver) {
       signBox.forEach(box => box.removeEventListener('click',_updateGameBoard))
     }
     console.log(isGameOver); 
@@ -101,13 +103,19 @@ const Gameboard = (function() {
   }
 })();
 
-
+// This is another Module for deciding a gameover.
 const displayController = (function() {
+  'use strict'
+  const openingPage = document.querySelector('.openingPage');
+
+
   const gameOver = () => {
     const gameBoard = Gameboard.gameBoard;
     console.log(gameBoard);
     console.log(gameBoard[0])
     let gameOver = false;
+    let isTie    = false;
+    // Check for the horizontal equation: 
     if (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2] && gameBoard[2] !== null) {
       gameOver = true;
     } else if (gameBoard[3] == gameBoard[4] && gameBoard[4] == gameBoard[5] && gameBoard[5] !== null) {
@@ -116,6 +124,7 @@ const displayController = (function() {
       gameOver = true;
     }
 
+    // Check for the vertical equation
     if (gameBoard[0] == gameBoard[3] && gameBoard[3] == gameBoard[6] && gameBoard[6] !== null) {
       gameOver = true;
     } else if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7] && gameBoard[7] !== null) {
@@ -124,19 +133,145 @@ const displayController = (function() {
       gameOver = true;
     }
 
+    // Check for the cross equation
     if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8] && gameBoard[8] !== null) {
       gameOver = true;
     } else if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6] && gameBoard[6] !== null) {
       gameOver = true;
     }
 
+    // Check for the tie
+    if (!gameBoard.includes(null)) {
+      console.log("It's a TIE!");
+      isTie    = true;
+      gameOver = true;
+    }
+
     if (gameOver) {console.log('GAME OVER!')};
-    return gameOver;
+    return {
+      gameOver,
+      isTie
+    }
   }
+
+  const _playerType = (event) => {
+    // We got 2 options: Single player or Multi-Player
+    let playerType;
+    if (event.target.id == 'singlePlayer') {
+      playerType = 'single';
+    } else {
+      playerType = 'multi';
+    }
+    console.log(playerType);
+    return playerType;
+  }
+  
+  const _goInputSection = () => {
+    const options = document.querySelectorAll('.options div button');
+    options.forEach(option => option.addEventListener('click', (event) => {
+      let type = _playerType(event)
+      // Clean the opening page so that the users enter information about 
+      const openingItems = openingPage.children;
+      openingPage.removeChild(openingItems[1])
+      // openingPage.removeChild(openingItems[0])
+      console.log('type:', type);  
+      type == 'single' ? _singlePlayer(): _multiPlayer()
+    }))
+  }
+  
+  const _singlePlayer = () => {
+    const header = document.querySelector('.openingHeader h1');
+    header.textContent = 'You will be playing against AI!'
+
+    const singleDiv = document.createElement('div');
+    singleDiv.classList.add('singleDiv');
+
+    const singleLabel = document.createElement('label');
+    singleLabel.setAttribute('name','single');
+    singleLabel.id = 'single';
+    singleLabel.textContent = 'Please Enter Your Name'
+
+    const singleInput = document.createElement('input');
+    singleInput.setAttribute('type','text');
+    singleInput.setAttribute('name','single');
+    singleInput.setAttribute('placeholder','Your Name');
+    singleInput.id = 'single';
+
+    const startBTN = document.createElement('button');
+    startBTN.classList.add('singleStartBtn');
+    startBTN.textContent = 'START'
+
+    singleDiv.appendChild(singleLabel);
+    singleDiv.appendChild(singleInput);
+    singleDiv.appendChild(startBTN);
+
+    openingPage.style.backgroundColor = 'rgb(53, 1, 53)';
+    // openingPage.style.color = '#d1c4e9';
+    openingPage.appendChild(singleDiv);
+  }
+
+  const _multiPlayer = () => {
+    const header = document.querySelector('.openingHeader h1');
+    header.textContent = 'MULTI PLAYER MODE!'
+
+    const multiDiv = document.createElement('div');
+    multiDiv.classList.add('multiDiv');
+
+    const player1Div = document.createElement('div');
+    player1Div.classList.add('player1Div'); // eklemesem de olur ? 
+    
+    const player1Label = document.createElement('label');
+    player1Label.setAttribute('name','player1Label');
+    player1Label.id = 'player1Label';
+    player1Label.textContent = 'FIRST PLAYER NAME'
+
+    const player1Input = document.createElement('input');
+    player1Input.setAttribute('type','text');
+    player1Input.setAttribute('name','player1Input');
+    player1Input.setAttribute('placeholder','Player 1');
+    player1Input.id = 'player1Input';
+
+    const player2Div = document.createElement('div');
+    player2Div.classList.add('player2Div'); // eklemesem de olur ? 
+
+    const player2Label = document.createElement('label');
+    player2Label.setAttribute('name','player2Label');
+    player2Label.id = 'player2Label';
+    player2Label.textContent = 'SECOND PLAYER NAME'
+
+    const player2Input = document.createElement('input');
+    player2Input.setAttribute('type','text');
+    player2Input.setAttribute('name','player2Input');
+    player2Input.setAttribute('placeholder','Player 2');
+    player2Input.id = 'player2Input';
+
+    const startBTN = document.createElement('button');
+    startBTN.classList.add('multiStartBtn');
+    startBTN.textContent = 'START'
+
+    player1Div.appendChild(player1Label);
+    player1Div.appendChild(player1Input);
+
+    player2Div.appendChild(player2Label);
+    player2Div.appendChild(player2Input);
+
+    multiDiv.appendChild(player1Div);
+    multiDiv.appendChild(player2Div);
+    multiDiv.appendChild(startBTN);
+
+    openingPage.appendChild(multiDiv);
+
+    openingPage.style.backgroundColor = 'rgb(53, 1, 53)';
+  }
+
+  const startGame   = () => {}
+
+  _goInputSection();
 
   return {
     gameOver
   }
+
 })()
 
 
